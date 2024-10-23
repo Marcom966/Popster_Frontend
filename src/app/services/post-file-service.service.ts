a89911365fc1a6b30a213ce37ff471085119bc9a
marcom966 <marcom962015@gmail.com>
2024-10-23 11:37:17 +0200

    Got error 404, trying to solve it

--- src/app/services/post-file-service.service.ts
+++ src/app/services/post-file-service.service.ts
@@ -9,7 +9,7 @@ export class PostFileServiceService {
 
   constructor(private http: HttpClient) { }
   public postFile(formData: FormData): Observable<any>{
-    const url = `http://localhost:8080/api/v1/file/`;
-    return this.http.post(url+`/Upload`, formData);
+    const url = `http://localhost:8080/api/v1/file`;
+    return this.http.post(url, formData);
   }
 }
