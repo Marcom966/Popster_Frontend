52438549ec03c4f418fabfe986abacc96fc2b6b6
marcom966 <marcom962015@gmail.com>
2025-01-16 20:57:07 +0100

    handle error

--- src/app/services/post-file-service.service.ts
+++ src/app/services/post-file-service.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +8,21 @@ import { Observable } from 'rxjs';
 export class PostFileServiceService {
 
   constructor(private http: HttpClient) { }
+  private handleError(error: HttpErrorResponse){
+    if(error.status===0){
+      console.error("an error occurred: "+error.error);
+    }else if(error.status===400){
+      let er = Object.values(error);
+      console.error(`Backend returned code ${error.status}, body was: `+er)
+    }else if(error.status===500){
+      console.error(error.message);
+    }
+    return throwError(()=>new Error('something bad happened'+error.status)); 
+
+  }
   public postFile(formData: FormData): Observable<any>{
     const url = `http://localhost:8080/api/v1/file`;
-    return this.http.post<any>(url, formData);
+    return this.http.post<any>(url, formData)
+    .pipe(catchError(this.handleError));
   }
 }
