e649dd2dd868f54468c3f181f7781568658b85ee
marcom966 <marcom962015@gmail.com>
2025-02-12 10:22:48 +0100

    add new cardDetail component

---
+++ src/app/components/card-detail-component/card-detail-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-detail-component',
+  imports: [],
+  templateUrl: './card-detail-component.component.html',
+  styleUrl: './card-detail-component.component.css'
+})
+export class CardDetailComponentComponent {
+
+}
