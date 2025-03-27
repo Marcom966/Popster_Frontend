fcbdaf3a1ce8fac2a014426d430659a3c43168c1
marcom966 <marcom962015@gmail.com>
2025-03-27 17:11:45 +0100

    moving a bit forward

--- src/app/components/audio-player-component/audio-player-component.component.ts
+++ src/app/components/audio-player-component/audio-player-component.component.ts
@@ -52,6 +52,8 @@ export class AudioPlayerComponentComponent {
   console.log('sto riproducendo:'+ this.name +' '+this.link);
   let audio = new Audio();
   audio.src = this.link;
+  console.log("it's returning a json probably"+this.file.stream());
+  
   audio.load();
   audio.volume = 1;
   audio.play().then(()=>{
