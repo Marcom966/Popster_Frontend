3e6bd46aeef949ab7e5c4e0b4e4527957cd87c4c
marcom966 <marcom962015@gmail.com>
2025-04-07 11:00:06 +0200

    the eroor is still there

--- src/app/components/audio-player-component/audio-player-component.component.ts
+++ src/app/components/audio-player-component/audio-player-component.component.ts
@@ -4,6 +4,7 @@ import { Subscription } from 'rxjs';
 import { Track } from 'ngx-audio-player';
 import { PostFileServiceService } from 'src/app/services/post-file-service.service';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-audio-player-component',
@@ -26,7 +27,7 @@ export class AudioPlayerComponentComponent {
   audioUrl: string | null = null;
   audio: HTMLAudioElement | null = null;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   public playAudio() {
     if(!this.link){
@@ -40,36 +41,48 @@ export class AudioPlayerComponentComponent {
         this.audio.pause();
         this.audio.src = '';
       }
-      this.audio = new Audio();
-      this.audio.addEventListener('error', (e) => {
-        console.error('Errore nel caricamento audio:', e);
-        console.error('URL dell\'audio:', this.link);
-        this.notRecognized = true;
-      });
-
-      this.audio.addEventListener('loadeddata', () => {
-        console.log('Audio caricato con successo');
-        this.audio!.volume = 1;
-        
-        if (this.pressPlay) {
-          this.audio!.play().then(() => {
-            console.log('audio played successfully');
-          }).catch(error => {
-            console.error('audio not played:', error);
-            this.pressPlay = true;
+      if (this.audioUrl) {
+        URL.revokeObjectURL(this.audioUrl);
+      }
+      this.http.get(this.link, { responseType: 'blob' }).subscribe(
+        (blob: Blob) => {
+          this.audioUrl = URL.createObjectURL(blob);
+          this.audio = new Audio();
+          this.audio.src = this.audioUrl;
+          
+          this.audio.addEventListener('error', (e) => {
+            console.error('Errore nel caricamento audio:', e);
+            this.notRecognized = true;
           });
-        }
-      });
-      this.audio.src = this.link;
-      this.audio.load();
-      this.playlist = [{
-        title: this.name,
-        link: this.link,
-        artist: 'demo',
-        duration: 0
-      }];
 
-      console.log('sto riproducendo:', this.name, 'URL:', this.link);
+          this.audio.addEventListener('loadeddata', () => {
+            console.log('Audio caricato con successo');
+            this.audio!.volume = 1;
+            
+            if (this.pressPlay) {
+              this.audio!.play().then(() => {
+                console.log('audio played successfully');
+              }).catch(error => {
+                console.error('audio not played:', error);
+                this.pressPlay = true;
+              });
+            }
+          });
+          this.audio.load();
+          this.playlist = [{
+            title: this.name,
+            link: this.audioUrl,
+            artist: 'demo',
+            duration: 0
+          }];
+
+          console.log('sto riproducendo:', this.name);
+        },
+        (error) => {
+          console.error('Errore nel fetch del file audio:', error);
+          this.notRecognized = true;
+        }
+      );
     } catch (error) {
       console.error('Errore nella creazione dell\'audio:', error);
       this.notRecognized = true;
@@ -84,6 +97,9 @@ export class AudioPlayerComponentComponent {
     if (this.audio) {
       this.audio.pause();
       this.audio.src = '';
+    }
+    if (this.audioUrl) {
+      URL.revokeObjectURL(this.audioUrl);
     }
   }
 }
