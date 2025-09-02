964a2c1cc04a9569e7b25c0fa36272f756ad29a4
marcom966 <marcom962015@gmail.com>
2025-09-02 11:59:54 +0200

    changes

--- src/app/components/card-detail-component/card-detail-component.component.ts
+++ src/app/components/card-detail-component/card-detail-component.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card-detail-component',
@@ -8,5 +9,16 @@ import { Component } from '@angular/core';
 })
 export class CardDetailComponentComponent {
   username!: string|null;
+  constructor(private route: Router) {}
+
+  public main(){
+    this.username = localStorage.getItem('user_name');
+  }
+  public backToHome(){
+    this.route.navigate(['/home']);
+  }
+  ngOnInit() {
+    this.main();
+  }
 
 }
