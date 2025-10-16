cf672a95345c28a279745845d8bf1508bcf57cc1
marcom966 <marcom962015@gmail.com>
2025-10-16 14:45:00 +0200

    card detail component 1

--- src/app/components/card-detail-component/card-detail-component.component.ts
+++ src/app/components/card-detail-component/card-detail-component.component.ts
@@ -1,6 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { Track } from 'ngx-audio-player';
+import { Subject, Subscription, takeUntil } from 'rxjs';
 import { FileDataInterface } from 'src/app/Interfaces/file-data-interface';
+import { StreamState } from 'src/app/Interfaces/stream-state';
+import { AudioPlayerServiceService } from 'src/app/services/audio-player-service.service';
 
 @Component({
   selector: 'app-card-detail-component',
@@ -19,7 +24,23 @@ export class CardDetailComponentComponent {
   dataUserName!: string|null;
   dataLink!: string|null;
   dataBlob!: string|null;
-  constructor(private route: Router) {}
+  playlist: Track[] = [];
+  requestSub = new Subscription();
+  linkDue!: string;
+  noLink: boolean = false;
+  pressPlay: boolean = false;
+  notRecognized: boolean = false;
+  state: StreamState = {
+    playing: false,
+    readableCurrentTime: '',
+    readableDuration: '',
+    duration: undefined,
+    currentTime: undefined,
+    canPlay: false,
+    error: false,
+    mute: false
+  };
+  constructor(private route: Router, private http: HttpClient, private audioService: AudioPlayerServiceService) {}
 
   public main(){
     this.username = localStorage.getItem('user_name');
@@ -30,14 +51,108 @@ export class CardDetailComponentComponent {
     this.dataUserName = localStorage.getItem('dataUserName');
     this.dataLink = localStorage.getItem('dataLink');
     this.dataBlob = localStorage.getItem('datablob');
-    console.log(this.dataBlob);
     
   }
+
+
+  private destroy$ = new Subject<void>();
+  private audioUrl: string | null = null;
+
+
+
+
+  public playAudio() {
+    if(!this.dataBlob){
+      console.error('nessun ID file disponibile');
+      this.noLink = true;
+      return    
+    }
+
+    console.log('Tentativo di riproduzione audio con ID:', this.dataBlob);
+
+    try {
+      const blob = this.dataBlob;
+      if (!blob) {
+      this.notRecognized = true;
+      return;
+      }
+      this.playStream(blob);
+    } catch (error){
+      console.error('Errore nella creazione dell\'URL:', error);
+      this.notRecognized = true;
+    }
+  }
+
+  playStream(url: string){
+    this.audioService.playStream(url)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next:(event: any)=>{
+          console.log('Evento di riproduzione:', event);
+          if(event.type === 'error'){
+            console.error('Errore durante la riproduzione:', event);
+            this.notRecognized = true;  
+          }
+        },
+        error: error=>{
+          console.error('Errore nella riproduzione:', error);
+          this.notRecognized = true;
+        }
+      });
+  }
+
+  pause() {
+    console.log('Pausa audio');
+    this.audioService.pause();
+  }
+
+  play() {
+    console.log('Ripresa audio');
+    this.audioService.play();
+  }
+
+  stop() {
+    console.log('Stop audio');
+    this.audioService.stop();
+  }
+
+  mute() {
+    console.log('Muto audio');
+    this.audioService.mute();
+  }
+
+  unmute() {
+    console.log('Unmute audio');
+    this.audioService.unmute();
+  }
+
+  onSliderChangeEnd(change: any) {
+    console.log('Cambio posizione audio:', change.value);
+    this.audioService.seekTo(change.value);
+  }
+
   public backToHome(){
     this.route.navigate(['/home']);
   }
+
+  ngOnDestroy() {
+    if (this.audioUrl) {
+      URL.revokeObjectURL(this.audioUrl);
+    }
+    this.destroy$.next();
+    this.destroy$.complete();
+    this.stop();
+  }
+
   ngOnInit() {
     this.main();
+    this.audioService.getState()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((state: StreamState) => {
+        this.state = state;
+        console.log('Stato audio aggiornato:', state);
+      });
   }
+
 
 }
