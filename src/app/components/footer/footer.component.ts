138f71f2a642242ab90fd7ddde612e6565c41931
marcom966 <marcom962015@gmail.com>
2025-11-27 10:13:43 +0100

    footer implementation

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-footer',
@@ -7,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
     standalone: false
 })
 export class FooterComponent implements OnInit {
+  username!: string|null;
 
-  constructor() { }
+  constructor(private route: Router) { }
 
+  public main():void{
+    this.username = localStorage.getItem('user_name');
+  }
+  public toUserPage():void{
+    this.route.navigate(['/userDetails']);
+  }
+  
   ngOnInit(): void {
+    this.main();
   }
 
 }
