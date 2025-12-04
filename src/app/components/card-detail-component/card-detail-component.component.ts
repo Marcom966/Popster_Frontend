2de1e6de30b36e2a1ee26ce7646e375ec98ae34c
marcom966 <marcom962015@gmail.com>
2025-12-04 17:39:11 +0100

    trying to fix card details bug

--- src/app/components/card-detail-component/card-detail-component.component.ts
+++ src/app/components/card-detail-component/card-detail-component.component.ts
@@ -42,6 +42,7 @@ export class CardDetailComponentComponent {
   faStopIcon!: any;
   faVolumeMuteIcon!: any;
   faVolumeUpIcon!: any;
+  deleted: boolean = false;
   state: StreamState = {
     playing: false,
     readableCurrentTime: '',
@@ -189,6 +190,7 @@ export class CardDetailComponentComponent {
         localStorage.removeItem('datasongname');
         localStorage.removeItem('datalink');
         localStorage.removeItem('datausername');
+        this.deleted = true;
       });
     } else {
       console.error('Error: dataId is null or undefined.');
