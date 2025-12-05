f9c6ce24b3e209372cd226119786eccea90b8601
marcom966 <marcom962015@gmail.com>
2025-12-05 09:58:45 +0100

    deleted deleted variable from file deleted component

--- src/app/components/card-detail-component/card-detail-component.component.ts
+++ src/app/components/card-detail-component/card-detail-component.component.ts
@@ -42,7 +42,6 @@ export class CardDetailComponentComponent {
   faStopIcon!: any;
   faVolumeMuteIcon!: any;
   faVolumeUpIcon!: any;
-  fileDeleted: boolean = false;
   state: StreamState = {
     playing: false,
     readableCurrentTime: '',
@@ -190,7 +189,6 @@ export class CardDetailComponentComponent {
         localStorage.removeItem('datasongname');
         localStorage.removeItem('datalink');
         localStorage.removeItem('datausername');
-        this.fileDeleted = true;
       });
     } else {
       console.error('Error: dataId is null or undefined.');
