2a1e01b4a388d08ba175d0b08e6df93fd3dadc1d
marcom966 <marcom962015@gmail.com>
2025-12-18 12:02:17 +0100

    Trying to build the user detail component

--- src/app/components/user-detail-component/user-detail-component.component.ts
+++ src/app/components/user-detail-component/user-detail-component.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { FetchUsersService } from 'src/app/services/fetch-users.service';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-user-detail-component',
@@ -10,15 +13,35 @@ import { Router } from '@angular/router';
 })
 export class UserDetailComponentComponent {
   isLoading: boolean = false;
+  username!: string;
+  requestSub = new Subscription();
+  constructor(private route: Router, private getTheuser: FetchUsersService) { }
 
-  constructor(private route: Router) { }
+  public main(){
+    this.username = localStorage.getItem('user_name') || '';
+    this.requestSub = this.getTheuser.returnSpecificUser(this.username)
+    .pipe(
+      catchError((error) => {
+        console.error('Error occurred:', error);
+        return throwError(() => new Error('An error occurred while fetching the user.'));
+      })
+    )
+    .subscribe(resp=>{
+      console.log(resp);
+    })
+    
+  }
 
-
-  backTotheHomepage() {
+  public backTotheHomepage() {
     this.route.navigate(['/homepage']);
   }
-  backtoUserFiles() {
+  public backtoUserFiles() {
     this.route.navigate(['/userHomepage']);
+  }
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.main();
   }
 
 }
