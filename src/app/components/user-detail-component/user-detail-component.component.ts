6dddb795183dcdf411b885991743a9c939faaa13
marcom966 <marcom962015@gmail.com>
2025-12-18 10:41:47 +0100

    user detail beginning

--- src/app/components/user-detail-component/user-detail-component.component.ts
+++ src/app/components/user-detail-component/user-detail-component.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-detail-component.component.html',
   styleUrl: './user-detail-component.component.css'
 })
 export class UserDetailComponentComponent {
+  isLoading: boolean = false;
+
+  constructor(private route: Router) { }
+
+
+  backTotheHomepage() {
+    this.route.navigate(['/homepage']);
+  }
+  backtoUserFiles() {
+    this.route.navigate(['/userHomepage']);
+  }
 
 }
